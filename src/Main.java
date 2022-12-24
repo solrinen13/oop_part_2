2dfca0704d8215fdd1c738ff8a10cde5d6043570
Saveliy <saveliy.kan@gmail.com>
2022-12-24 15:46:54 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import Transport.Car;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car ("Лада","Гранта",1.7,"желтого",2015,"России");
-        Car audiA8 = new Car ("Ауди","A8",3.0,"черного",2020,"Германии");
-        Car bmw_Z8 = new Car("BMW", "Z8",3.0,"черного",2021,"Германии");
-        Car kiaSportageFour = new Car("Кия","Спортайдж 4-поколения",2.4,"красного",2018,"Южной корее");
-        Car hyundayAvante = new Car("Хендай", "Авант", 1.6, "оранжевого", 2016, "Южной Кореи");
+        Car ladaGranta = new Car ("Лада","Гранта",1.7,"желтого",2015,"России",
+                "front","sports",231,2,true);
+        Car audiA8 = new Car ("Ауди","A8",3.0,"черного",2020,"Германии",
+                "fool","sports",2123,4,true);
+        Car bmw_Z8 = new Car("BMW", "Z8",3.0,"черного",2021,"Германии","auto",
+                "sports",342,4,false);
+        Car kiaSportageFour = new Car("Кия","Спортайдж 4-поколения",2.4,"красного",2018,"Южной корее",
+                "auto","cuv",122,6,false);
+        Car hyundayAvante = new Car("Хендай", "Авант", 1.6, "оранжевого", 2016, "Южной Кореи"
+        ,"front","sedan",421,4,true);
         System.out.println(ladaGranta);
-        System.out.println(audiA8);
+      /*  System.out.println(audiA8);
         System.out.println(bmw_Z8);
         System.out.println(kiaSportageFour);
-        System.out.println(hyundayAvante);
+        System.out.println(hyundayAvante);*/
     }
 }
